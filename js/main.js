26453828fc9e32994977181cea17d010141f47d8
Néstor Oubiña <nestor.j.o.a@gmail.com>
2024-04-22 16:40:41 -0400

    Refactor main.js to populate data from URL parameters and validate ID number

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 var parser = new DOMParser();
+let xmlAvalista = "";
 
 function makeRequest(method, url) {
   return new Promise(function (resolve, reject) {
@@ -24,9 +25,31 @@ function makeRequest(method, url) {
   });
 }
 
+async function populateData() {
+  // Get the values from the URL parameters
+  // The parameters are three strings: elecciones, circunscripcion and partido
+  // TESTING URL: http://127.0.0.1:3000/index.html?partido=VOLT%20espa%C3%B1a&circunscripcion=Circunscripci%C3%B3n%20%C3%BAnica&elecciones=PARLAMENTO%20EUROPEO&email=nestor.j.o.a@gmail.com
+
+  const urlParams = new URLSearchParams(window.location.search);
+  const elecciones = urlParams.get("elecciones");
+  const circunscripcion = urlParams.get("circunscripcion");
+  const partido = urlParams.get("partido");
+  const email = urlParams.get("email");
+
+  // Change all the elements in the DOM with the values from the URL parameters
+  let x = document.getElementsByClassName("page");
+  var inner = x[0].innerHTML;
+  inner = inner.replace(/@elecciones@/gi, elecciones);
+  inner = inner.replace(/@circunscripcion@/gi, circunscripcion);
+  inner = inner.replace(/@partido@/gi, partido);
+  inner = inner.replace(/@email@/gi, email);
+  x[0].innerHTML = inner;
+}
+
 async function doRequests() {
   var candidatura = await makeRequest("GET", "xml/candidatura.xml");
-  console.log(candidatura);
+  await console.log(candidatura);
+  populateData();
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -53,6 +76,12 @@ async function populateAvalistaXml() {
     }
   });
 
+  if (validarId(document.getElementById("id").value) === false) {
+    isFormValid = false;
+    console.log("El número de documento no es válido.");
+    xmlPreviewSpan.textContent += "El número de documento no es válido.\n";
+  }
+
   if (!isFormValid) {
     console.log("Please fill in all required fields.");
     xmlPreviewSpan.textContent += "Por favor, rellene todos los campos.\n";
@@ -63,12 +92,12 @@ async function populateAvalistaXml() {
   var avalista = document.implementation.createDocument("", "", null);
 
   // Get form values
-  var nombre = document.getElementById("nomb").value;
-  var apellido1 = document.getElementById("ape1").value;
-  var apellido2 = document.getElementById("ape2").value;
+  var nombre = document.getElementById("nomb").value.toUpperCase();
+  var apellido1 = document.getElementById("ape1").value.toUpperCase();
+  var apellido2 = document.getElementById("ape2").value.toUpperCase();
   var fechaNacimiento = document.getElementById("fnac").value;
   var tipoDocumento = document.getElementById("tipoid").value;
-  var numeroDocumento = document.getElementById("id").value;
+  var numeroDocumento = document.getElementById("id").value.toUpperCase();
 
   // Create XML elements and append them to doc
   var avalistaElement = avalista.createElement("avalista");
@@ -100,9 +129,9 @@ async function populateAvalistaXml() {
 
   // Print XML to textarea
   var serializer = new XMLSerializer();
-  var xmlString = serializer.serializeToString(avalista);
+  xmlAvalista = serializer.serializeToString(avalista);
 
-  xmlPreviewSpan.textContent = xmlString;
+  xmlPreviewSpan.textContent = xmlAvalista;
 }
 
 function invokeSign() {
@@ -116,4 +145,22 @@ function invokeSign() {
   insertEnvelopedSignatureOnNodeByXPath(xmlDoc, "/oce/avalcandidatura");
   let dataB64 = AutoScript.getBase64FromXML(xmlDoc);
   AutoScript.sign(dataB64, "SHA512withRSA", "XAdES");
+}
+
+function validarId(value) {
+  var validChars = "TRWAGMYFPDXBNJZSQVHLCKET";
+  var nifRexp = /^[0-9]{8}[TRWAGMYFPDXBNJZSQVHLCKET]$/i;
+  var nieRexp = /^[XYZ][0-9]{7}[TRWAGMYFPDXBNJZSQVHLCKET]$/i;
+  var str = value.toString().toUpperCase();
+
+  if (!nifRexp.test(str) && !nieRexp.test(str)) return false;
+
+  var nie = str.replace(/^[X]/, "0").replace(/^[Y]/, "1").replace(/^[Z]/, "2");
+
+  var letter = str.substr(-1);
+  var charIndex = parseInt(nie.substr(0, 8)) % 23;
+
+  if (validChars.charAt(charIndex) === letter) return true;
+
+  return false;
 }
